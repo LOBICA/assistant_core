b484fa959070dc78bc5dc31be6d0b105f14ba8e8
Andres Javier Lopez <code@andresjavierlopez.com>
2025-08-26 15:46:21 -0600

    Add default states

---
+++ assistant_core/state.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+
+from langgraph.graph import MessagesState
+
+
+def replace(old, new):
+    return new
+
+
+class QuestionState(MessagesState):
+    question: str | None = None
+    answer: str | None = None
+
+
+class NextProcessState(MessagesState):
+    next: Annotated[str | None, replace] = None
---
+++ tests/unit/test_states.py
@@ -0,0 +1,49 @@
+from typing import Annotated, get_args, get_type_hints
+from unittest.mock import Mock
+
+from langgraph.graph import MessagesState
+
+from assistant_core.state import NextProcessState, QuestionState
+
+
+def assign_to_state(State, values):
+    """Emulate graph state assignment"""
+    state = State()
+    hints = get_type_hints(State, include_extras=True)
+    for key, value in values.items():
+        annot = hints.get(key)
+        reducer = get_args(annot)[1]
+        if reducer is type(None):
+            state[key] = value
+        else:
+            state[key] = reducer(state.get(key), value)
+    return state
+
+
+def test_reducer_call():
+    """Test that our state assigment is working as expected"""
+    reducer = Mock()
+    reducer.return_value = "test2"
+
+    class TestState(MessagesState):
+        attribute: Annotated[str, reducer] = None
+
+    state = assign_to_state(TestState, {"attribute": "test1"})
+
+    assert state["attribute"] == "test2"
+    reducer.assert_called_once_with(None, "test1")
+
+
+def test_question_state():
+    state = assign_to_state(
+        QuestionState, {"question": "How are you?", "answer": "I am fine."}
+    )
+
+    assert state["question"] == "How are you?"
+    assert state["answer"] == "I am fine."
+
+
+def test_next_process_state():
+    state = assign_to_state(NextProcessState, {"next": "test"})
+
+    assert state["next"] == "test"
